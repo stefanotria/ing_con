04b79f2bf349b2b59b8f3cf7cdbad35788602cb1
Tommaso Lionetti <tommaso_1998@libero.it>
2020-01-23 17:16:00 +0100

    Inserita funziona di predizione di un'immagine
    - Inserita funzione di predizione di un'immagine (inserita dall'utente)
    - Inserito supporto al file CSV per poter decifrare la classe predetta e il codice dell'opera

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from PIL import Image
+
+import numpy as np
+
 from queryManager import Query
 from recognition import Recognition
 from dataset import Dataset
@@ -5,12 +9,21 @@ from dataset import Dataset
 ds = Dataset(5)
 rec = Recognition(ds)
 
-predictions = rec.predictImages()
+# predictions = rec.predictImages()
+# print("Ecco le opere riconosciute")
+# print(predictions)
 
-print("Ecco le opere riconosciute")
-print(predictions)
-print("Cerco informazioni per: ", predictions[0])
-query = Query(predictions[0])
+picture = raw_input("Inserisci il nome dell'immagine da predire : ") #picture.jpg
+image = Image.open(picture)
+predicted_class = rec.predictImage(image)
+
+print(predicted_class)
+
+predicted_label = ds.getLabelByClass('Artwork', predicted_class) #Restituisce il nome
+ds.getLabelByClass('Code', predicted_class) #Restituisce il codice
+
+print "Cerco informazioni per: " + predicted_label
+query = Query(predicted_label)
 query.getAuthor()
 query.getMuseum()
 query.getDate()
