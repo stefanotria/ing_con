d35ecdeb39e66fabe12c2439eadd5b8e2b4eef3e
Francesco Scagliusi <frank.sca98@gmail.com>
2020-01-23 11:44:43 +0100

    Aggiunta struttura per la generazione di query automatiche e la loro visualizzazione. Implementato modulo per salvare il modello appreso dalla rete neurale

--- recognition.py
+++ recognition.py
@@ -1,66 +1,38 @@
 # Classe contenente il riconoscitore delle immagini
 
-import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-import os
+from tensorflow.keras.models import load_model
+from PIL import Image
+from pathlib import Path
 import numpy as np
-import matplotlib.pyplot as plt
 
 class Recognition:
 
-    def __init__(self):
-        self.total_val = 0
-        self.total_train = 0
-        self.batch_size = 6
-        self.epochs = 15
-        self.IMG_HEIGHT = 64
-        self.IMG_WIDTH = 64
+    model = ""
+    dataset = ""
+    epochs = 15
 
-    def loadDataset(self):
-        PATH = 'paintings'
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.train_data_gen = dataset.generateTrainData()
+        self.val_data_gen = dataset.generateTestData()
+        self.initializeModel()
 
-        train_dir = os.path.join(PATH, 'train')
-        validation_dir = os.path.join(PATH, 'test')
-
-        self.classes = os.listdir(os.path.join(PATH, 'train'))
-
-        for n in self.classes:
-            self.total_train += len(os.listdir(os.path.join(train_dir, n)))
-        for n in self.classes:
-            self.total_val += len(os.listdir(os.path.join(validation_dir, n)))
-
-        self.generateTrainData(train_dir)
-        self.generateTestData(validation_dir)
-
-    def generateTrainData(self, train_dir):
-        train_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our training data
-
-        self.train_data_gen = train_image_generator.flow_from_directory(batch_size=self.batch_size,
-                                                                        directory=train_dir,
-                                                                        shuffle=True,
-                                                                        target_size=(self.IMG_HEIGHT, self.IMG_WIDTH),
-                                                                        class_mode='categorical')
-        sample, _ = next(self.train_data_gen)
-        # plotImages(sample_training_images[:5])
-
-    def generateTestData(self, validation_dir):
-        validation_image_generator = ImageDataGenerator(rescale=1. / 255)  # Generator for our validation data
-
-        self.val_data_gen = validation_image_generator.flow_from_directory(batch_size=self.batch_size,
-                                                                           directory=validation_dir,
-                                                                           target_size=(
-                                                                           self.IMG_HEIGHT, self.IMG_WIDTH),
-                                                                           class_mode='categorical')
-        sample, _ = next(self.val_data_gen)
-
-        self.sample_test_images = sample
+    def initializeModel(self):
+        my_file = Path("model.h5")
+        if my_file.is_file():
+            # il file esiste
+            self.loadModel("model")
+        else:
+            self.defineModel()
+            self.saveModel("model")
+            self.compileModel()
+            self.fitModel()
 
     def defineModel(self):
-        model = Sequential([
-            Conv2D(16, 3, padding='same', activation='relu', input_shape=(self.IMG_HEIGHT, self.IMG_WIDTH, 3)),
+        self.model = Sequential([
+            Conv2D(16, 3, padding='same', activation='relu', input_shape=(self.dataset.IMG_HEIGHT, self.dataset.IMG_WIDTH, 3)),
             MaxPooling2D(),
             Conv2D(32, 3, padding='same', activation='relu'),
             MaxPooling2D(),
@@ -68,47 +40,56 @@ class Recognition:
             MaxPooling2D(),
             Flatten(),
             Dense(512, activation='relu'),
-            Dense(len(self.classes), activation='softmax')
+            Dense(len(self.dataset.classes), activation='softmax')
         ])
 
-        model.compile(optimizer='adam',
+    def compileModel(self):
+        self.model.compile(optimizer='adam',
                       loss='binary_crossentropy',
                       metrics=['accuracy'])
 
-        history = model.fit_generator(
+    def fitModel(self):
+        total_train = self.train_data_gen.samples
+        total_val = self.val_data_gen.samples
+        self.model.fit_generator(
             self.train_data_gen,
-            steps_per_epoch=self.total_train // self.batch_size,
+            steps_per_epoch=total_train // self.dataset.batch_size,
             epochs=self.epochs,
             validation_data=self.val_data_gen,
-            validation_steps=self.total_val // self.batch_size
+            validation_steps=total_val // self.dataset.batch_size
         )
 
-        return model
+    def loadImg(self, filename):
+        np_image = Image.open(filename)
+        new_image = np_image.resize((self.dataset.IMG_WIDTH, self.dataset.IMG_HEIGHT))
+        np_image = np.array(new_image).astype('float32') / 255
+        np_image = np_image.reshape((64, 64, 3))
+        return np_image
 
-    def predictImages(self, model):
-        self.plotImages(self.sample_test_images[:self.total_val])
+    def predictImages(self):
+        sample, _ = next(self.val_data_gen)
+        sample_test_images = sample
+        total_val = self.val_data_gen.samples
+        self.dataset.plotImages(sample_test_images[:total_val], total_val)
 
-        STEP_SIZE_TEST = self.val_data_gen.n // self.val_data_gen.batch_size
+        STEP_SIZE_TEST = self.val_data_gen.n // self.dataset.batch_size
         self.val_data_gen.reset()
-        pred = model.predict(self.val_data_gen,
-                                       steps=STEP_SIZE_TEST,
-                                       verbose=1)
+        pred = self.model.predict(self.val_data_gen,
+                             steps=STEP_SIZE_TEST,
+                             verbose=1)
 
         predicted_class_indices = np.argmax(pred, axis=1)
 
         labels = self.train_data_gen.class_indices
-        #print(labels)
+        # print(labels)
         labels = dict((v, k) for k, v in labels.items())
 
         predictions = [labels[k] for k in predicted_class_indices]
 
         return predictions
 
-    def plotImages(self, images_arr):
-        fig, axes = plt.subplots(1, self.total_val, figsize=(7, 7))
-        axes = axes.flatten()
-        for img, ax in zip(images_arr, axes):
-            ax.imshow(img)
-            ax.axis('off')
-        plt.tight_layout()
-        plt.show()
+    def saveModel(self, nome):
+        self.model.save(nome + ".h5")
+
+    def loadModel(self, nome):
+        self.model = load_model(nome + ".h5")
