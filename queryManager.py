6e8e7997b1ddd79a0d6ddbd5837aaa9d66f3db59
Francesco Scagliusi <frank.sca98@gmail.com>
2020-01-28 19:28:57 +0100

    Fixati i risultati delle query

--- queryManager.py
+++ queryManager.py
@@ -11,57 +11,62 @@ class Query:
         self.paint = paint
         self.query = ""
         self.select = ""
-        self.where = """?Paint wdt:P31 wd:Q3305213 .\n
-                     ?Paint rdfs:label ?Name FILTER regex(?Name, '""" + paint + """' ) ."""
+        self.where = """wd:""" + paint + """ rdfs:label ?Paint ."""
+        self.filter = "FILTER(lang(?Paint)='en')\n"
 
     # restituisce il nome dell'autore del dipinto
     def getAuthor(self):
         self.select += "?Author "
-        self.where += """OPTIONAL{?Paint wdt:P170 ?a .}
+        self.where += """OPTIONAL{wd:""" + self.paint + """ wdt:P170 ?a .}
               OPTIONAL{?a rdfs:label ?Author .}"""
+        self.filter += "FILTER(lang(?Author)='en')\n"
 
     # restituisce la data in cui e' stata realizzata l'opera
     def getDate(self):
         self.select += "?Date "
-        self.where += "OPTIONAL{?Paint wdt:P571 ?Date .} \n"
+        self.where += "OPTIONAL{wd:" + self.paint +  " wdt:P571 ?Date .} \n"
 
     # restituisce il nome del museo in cui e' custodita l'opera
     def getMuseum(self):
         self.select += "?Museum "
-        self.where += """OPTIONAL{?Paint wdt:P276 ?m .}
+        self.where += """OPTIONAL{wd:""" +self.paint + """ wdt:P276 ?m .}
               OPTIONAL{?m rdfs:label ?Museum .}\n"""
+        self.filter += "FILTER(lang(?Museum)='en')\n"
 
     # restituisce il movimento del dipinto
     def getMovement(self):
         self.select += "?Movement "
-        self.where += """OPTIONAL{?Paint wdt:P135  ?mov .}
+        self.where += """OPTIONAL{wd:""" +self.paint + """ wdt:P135  ?mov .}
                         OPTIONAL{?mov rdfs:label ?Movement .}\n"""
+        self.filter += "FILTER(lang(?Movement)='en')\n"
 
     # restituisce il genere del dipinto
     def getGenre(self):
         self.select += "?Genre"
-        self.where += """OPTIONAL{?Paint wdt:P136 ?gen .}
+        self.where += """OPTIONAL{wd:""" + self.paint + """ wdt:P136 ?gen .}
                         OPTIONAL{?gen rdfs:label ?Genre .}\n"""
+        self.filter += "FILTER(lang(?Genre)='en')\n"
 
     # restituisce altezza e larghezza del dipinto
     def getDimension(self):
         self.select += "?Height ?Width"
-        self.where += """OPTIONAL{?Paint wdt:P2048  ?Height .}
-                        OPTIONAL{?Paint wdt:P2049 ?Width .}\n"""
+        self.where += """OPTIONAL{wd:"""+ self.paint + """ wdt:P2048  ?Height .}
+                        OPTIONAL{wd:"""+self.paint+ """ wdt:P2049 ?Width .}\n"""
 
     def buildUp(self):
         self.query = "SELECT " + self.select
-        self.query += "\nWHERE {\n" + self.where + "\nSERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. }\n"
+        self.query += "\nWHERE {\n" + self.where + "\n"
+        self.query += self.filter
         self.query += "} LIMIT 1 "
 
-    def getInfo(self):
+    def getInfo(self, name):
         query = """ 
                 PREFIX dbo: <http://dbpedia.org/ontology/>
                 PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
                 SELECT ?Description {
                 ?opera a dbo:Artwork .
-                ?opera rdfs:label ?Name FILTER regex(?Name, '""" + self.paint + """') FILTER (lang(?Name) = "en")
-                ?opera dbo:abstract ?Description FILTER (lang(?Name) = "en") .
+                ?opera rdfs:label ?Name FILTER regex(?Name, '""" + name + """') FILTER (lang(?Name) = "en")
+                ?opera dbo:abstract ?Description FILTER (lang(?Description) = "en") .
                 } LIMIT 1"""
         results = self.setQuery(query, self.db)
         response = {}
@@ -73,7 +78,6 @@ class Query:
 
     def setQuery(self, query, wrapper):
         wrapper.setQuery(query)
-
         wrapper.setReturnFormat(JSON)
         results = wrapper.query().convert()
         return results
